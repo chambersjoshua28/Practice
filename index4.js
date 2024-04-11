001f27005d4dc8048978b30e037253eb1ab0ae22
Joshua <Chambersjoshua28@gmail.com>
2024-04-10 20:54:10 -0700

    Initial commit

---
+++ index4.js
@@ -0,0 +1,13 @@
+function myFunction () {
+    for (let i = 0; i < 100; i++) {
+        console.log(i);
+    }
+}
+
+myFunction(); 
+
+function createFullName(firstName, lastName) {
+    console.log(firstName+ ' '+ lastName); 
+}
+
+createFullName('Tom', 'Sawyer'); 
